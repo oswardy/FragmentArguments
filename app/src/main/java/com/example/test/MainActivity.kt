f647994fed6f0651b0dcfde9c16a620555798288
osward <oswardyong@gmail.com>
2021-08-09 01:08:47 +0800

    Added AboutActivity and Intent

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.test
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn : Button = findViewById(R.id.btnAbout)
+
+        btn.setOnClickListener(){
+            //call the aboutactivity
+            //val intent : Intent = Intent(this, AboutActivity::class.java)
+
+            //open google map
+            //val geo = Uri.parse("geo: 3.1390, 101.6869")
+            //val intent : Intent = Intent(Intent.ACTION_VIEW,geo)
+
+            //share
+            //val intent = Intent(Intent.ACTION_SEND)
+            //intent.setType("text/plain")
+            //intent.putExtra(Intent.EXTRA_TEXT,"hello")
+
+            //phone call
+            val tel= Uri.parse("tel:012345678")
+            val intent: Intent = Intent(Intent.ACTION_DIAL,tel)
+
+            startActivity(intent)
+        }
     }
 }
